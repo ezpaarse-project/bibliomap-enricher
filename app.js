9eae2e2b189ba6be79814a9383d49a57cb37dcad
sfmax54 <narko_54@hotmail.fr>
2017-06-09 17:38:10 +0200

    Can now start from a local docker image

--- app.js
+++ app.js
@@ -82,25 +82,13 @@ function createJob(streamName) {
   }
 
   printLog(`Creating an ezPAARSE job for ${streamName} at ${config.ezpaarse.url}`);
-
-   var headers = {};
-
-   if(process.env.BBE_LOGSTREAMS){
-   
-	switch(process.env.BBE_LOGSTREAMS){
-	case "_ISTEX_":
-	  headers = config.ezpaarse.headers.ISTEX;
-	  break;
-	case "_CNRS_":
-	  headers = config.ezpaarse.headers.BIBCNRS;
-	  break;
-	}
-   }
+ 
+  console.log(JSON.stringify(config.ezpaarse.headers));
 
   const job = {
     request: request.post({
       url: config.ezpaarse.url,
-      headers: headers
+      headers: config.ezpaarse.headers
     }),
     writeStream: new PassThrough()
   };
